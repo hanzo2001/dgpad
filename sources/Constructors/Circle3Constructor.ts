f323a9b3c69e32c7ef52aeed7496a0566ad865ae
hanzo2001 <bilbolord2001@gmail.com>
2016-08-10 21:13:00 +0100

    fix formatting

--- sources/Constructors/Circle3Constructor.ts
+++ sources/Constructors/Circle3Constructor.ts
@@ -11,41 +11,41 @@ export class Circle3Constructor extends ObjectConstructor {
 		// $U.extend(this, new ObjectConstructor()); //Héritage
 	}
 	getCode(): string {
-			return "circle3";
+		return "circle3";
 	}
 	getInitials(): string[] {
-			return ["point"];
+		return ["point"];
 	}
 	isLastObject(): boolean {
-			var c = this.getCList();
-			return c.length === 3;
+		var c = this.getCList();
+		return c.length === 3;
 	}
-	newObj(_zc, _C): Circle3Object {
-			return new Circle3Object(_zc.getConstruction(), "_C", _C[0], _C[1], _C[2]);
+	newObj(_zc, _C):Circle3Object {
+		return new Circle3Object(_zc.getConstruction(), "_C", _C[0], _C[1], _C[2]);
 	}
 	preview(events:MouseEvent, zc:iCanvas) {
-			var ctx = zc.getContext();
-			ctx.strokeStyle = zc.prefs.color.hilite;
-			ctx.lineWidth = zc.prefs.size.line;
-			var c = this.getCList();
-			var len = c.length;
-			var r;
-			ctx.beginPath();
-			switch (len) {
-					case 1:
-							r = $U.computeRay(this.getC(0).getX(), this.getC(0).getY(), zc.mouseX(events), zc.mouseY(events));
-							ctx.arc(zc.mouseX(events), zc.mouseY(events), r, 0, Math.PI * 2, true);
-							break;
-					case 2:
-							r = $U.computeRay(this.getC(0).getX(), this.getC(0).getY(), this.getC(1).getX(), this.getC(1).getY());
-							if (this.isSelectCreatePoint) {
-									ctx.arc(this.getC(1).getX(), this.getC(1).getY(), r, 0, Math.PI * 2, true);
-									this.isSelectCreatePoint = false;
-							} else {
-									ctx.arc(zc.mouseX(events), zc.mouseY(events), r, 0, Math.PI * 2, true);
-							}
-							break;
-			}
-			ctx.stroke();
+		var ctx = zc.getContext();
+		ctx.strokeStyle = zc.prefs.color.hilite;
+		ctx.lineWidth = zc.prefs.size.line;
+		var c = this.getCList();
+		var len = c.length;
+		var r;
+		ctx.beginPath();
+		switch (len) {
+			case 1:
+				r = $U.computeRay(this.getC(0).getX(), this.getC(0).getY(), zc.mouseX(events), zc.mouseY(events));
+				ctx.arc(zc.mouseX(events), zc.mouseY(events), r, 0, Math.PI * 2, true);
+				break;
+			case 2:
+				r = $U.computeRay(this.getC(0).getX(), this.getC(0).getY(), this.getC(1).getX(), this.getC(1).getY());
+				if (this.isSelectCreatePoint) {
+					ctx.arc(this.getC(1).getX(), this.getC(1).getY(), r, 0, Math.PI * 2, true);
+					this.isSelectCreatePoint = false;
+				} else {
+					ctx.arc(zc.mouseX(events), zc.mouseY(events), r, 0, Math.PI * 2, true);
+				}
+				break;
+		}
+		ctx.stroke();
 	}
 }
