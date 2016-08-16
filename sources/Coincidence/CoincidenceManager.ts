40ff80fbaae7f75fb12656889ef04fcba4bcccf7
hanzo2001 <bilbolord2001@gmail.com>
2016-08-16 21:18:11 +0100

    format code, remove comments

--- sources/Coincidence/CoincidenceManager.ts
+++ sources/Coincidence/CoincidenceManager.ts
@@ -15,46 +15,41 @@ export class CoincidenceManager implements iCoincidenceManager {
 		this.event = null;
 	}
 	checkCoincidences(event:MouseEvent): boolean {
+		let Cn = this.canvas.getConstruction();
 		// On récupère dans un tableau tous les objets situés
 		// sous le clic de souris ou le tap :
-		var t = this.canvas.getConstruction().getObjectsUnderMouse(event);
+		var t = Cn.getObjectsUnderMouse(event);
 		if (t.length < 2) {return false;}
 		// On va trier le tableau par famille d'objets :
 		var len = t.length;
-		for (var i = 0; i < len; i++) {
-			t[i].Scratch = this.priority(t[i]);
-		}
+		let i=0, s=t.length;
+		while (i<s) {t[i++].Scratch = this.priority(t[i]);}
 		t.sort(this.sortFilter);
-		var i = 1;
-		while ((i < len) && (t[i].Scratch === t[0].Scratch)) {i++;}
+		i=1;
+		while (i<s && t[i].Scratch === t[0].Scratch) {i++;}
 		// On ne va garder dans ce tableau que les objets d'une même
 		// famille (selon la priorité définie dans priority) :
 		t = t.slice(0, i);
 		if (t.length < 2) {return false;}
 		// Il faut ensuite que les objets soient confondus :
-		if (this.canvas.getConstruction().isMode(1)) {
-			for (var i = 1, len = t.length; i < t.length; i++) {
-				if (!t[0].isCoincident(t[i])) {return false;}
-			}
+		if (Cn.isMode(1)) {
+			i=1, s=t.length;
+			while (i<s) {if (!t[0].isCoincident(t[i++])) {return false;}}
 		}
 		// Si on arrive jusqu'ici, c'est qu'il y a ambiguité :
-		for (var i = 0; i < t.length; i++) {
+		i=0, s=t.length;
+		while (i<s) {
 			t[i] = [t[i].getName() + ': ' + $L.object[t[i].getCode()], t[i]];
-			if (t[i][1].isHidden()) t[i].push('#777');
-			else t[i].push(t[i][1].getColor().getHEX());
+			let color = t[i][1].isHidden() ? '#777' : t[i][1].getColor().getHEX();
+			t[i++].push(color);// this is a very mixed array
 		}
 		this.event = event;
 		this.panel = new BubblePanel(this.canvas, (a)=>this.exec(a), ()=>this.close(), event, t, $L.coincidence_message + ' : ' + $L.coincidence_select.replace('$1', t.length), 270, 190, 50);
-		// var cp = new CoincidencePanel(this.canvas, ev, t);
-		// for (var i = 0, len = t.length; i < t.length; i++) {
-		// 	console.log(t[i].getName() + ' ' + t[i].Scratch);
-		// }
 		return true;
-		// console.log(t);
 	}
 	isVisible(): boolean {
 		return (this.panel && this.panel.isVisible());
-	};
+	}
 	close() {
 		this.panel = null;
 	}
